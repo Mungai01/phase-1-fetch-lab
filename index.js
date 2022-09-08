44e853b238898f1fe5d72630e0d47c9491fd6720
Mungai01 <hoseahnm2@gmail.com>
2022-09-08 10:24:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
   
 }
+return fetch("https://anapioficeandfire.com/api/books")
+.then(response => response.json())
+.then(data => {
+  renderBooks(data);
+});
+.then(renderBooks);
+
+}
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -15,3 +24,5 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+
